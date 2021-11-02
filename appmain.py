b6113b35d65ae2b48c1bf9d008d0764740790815
eusoubrasileiro <eusoubrasileiro@gmail.com>
2021-11-02 20:59:52 -0300

    bokeh plotting memorial directions with arrows and circles

--- appmain.py
+++ appmain.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, request, jsonify
 import sys
 from poligonal.util import memorialRead, formatMemorial
+import json 
+
+from bokeh.plotting import figure
+from bokeh.embed import json_item
+from bokeh.models import Arrow, NormalHead
+
 
 app = Flask('careas-tools')
 
@@ -10,15 +16,58 @@ def convert():
         #print(request.json.encode('ascii', 'ignore'), file=sys.stdout, flush=True)
         #print(type(request.json), file=sys.stdout, flush=True)
         # Serialize the result, you can add additional fields
-        outtext = u"Insira o arquivo texto de entrada à esquerda"
+        outputfile = u"Insira o arquivo texto de entrada à esquerda"
         try: 
-            intext = memorialRead(request.json)
-            outtext = formatMemorial(intext)
+            inputfile = memorialRead(request.json)
+            # for plotting memorial 
+            coordinates = memorialRead(request.json, decimal=True)
+            json_plot = bokeh_memorial_draw(coordinates)
+            # output file formatted
+            outputfile = formatMemorial(inputfile)
         except Exception as excp: 
-            return jsonify(text=str(Exception), result='failed') 
+            return jsonify(text=str(Exception.__name__), result='failed')
         else:    
-            return jsonify(text=outtext, result='success')
+            return jsonify(text=outputfile, result='success', json_item=json_plot) 
     return None
+
+
+def bokeh_memorial_draw(coordinates):
+    """draw with bokeh the coordinates passed as circles
+    connected with arrows (according to the their order)
+    * returns: div html and script tag for embedding """
+    x, y = coordinates[:, 1], coordinates[:, 0]
+    #output_file("multiple.html")
+
+    TOOLTIPS = [ # allowed hover tooltips
+        ("index", "$index"),
+        ("(x,y)", "($x, $y)") 
+        ]
+    p = figure(width=300, height=300, toolbar_location="below",  tooltips=TOOLTIPS,
+        tools='box_zoom,pan,save,hover,reset,tap,wheel_zoom')
+    # draw all coordinates as circles 
+    p.circle(x, y, fill_color="gray", size=8, alpha=0.8)
+    #p.line(x, y, line_width=1)    
+    # add both a line and circles on the same plot
+    x0, y0 = x[0], y[0]
+    for x, y in zip(x[1:], y[1:]):
+        p.add_layout(Arrow(end=NormalHead(fill_color="white", size=10, 
+                    fill_alpha=0.2, line_width=1),
+                    x_start=x0, y_start=y0, x_end=x, y_end=y))
+        x0, y0 = x, y        
+    return json_item(p) # creates a json package w. my plot
+
+
+# not needed for now
+PageState = {} # state dictionary for the page
+
+@app.route('/state', methods=['GET', 'POST'])
+def state():
+    # if request.method == 'POST':
+    #     return jsonify(text=str(Exception.__name__), result='failed')
+    # else:
+    #     return jsonify(text=outputfile, result='success', json_item=json_plot) 
+    return None
+
 
 @app.route('/')
 def index():
--- static/main.js
+++ static/main.js
@@ -1,68 +1,75 @@
 
-var intext = document.getElementById("infile");
-var outext = document.getElementById("outfile");
 
-function onLoad(){
+
+var inputtext = document.getElementById("infile");
+var outputtext = document.getElementById("outfile");
+var divgraph = document.getElementById('bokeh_plot');
+var fileUploader = document.getElementById('file-uploader');
+
+
     // sample input file
-    intext.value =
+var sample_inputfile = 
 `  -19°44'18''174 -44°17'41''703||
 -19;44;;18''174 -44°17'45''410
  xxxx -19°44'16''507 -44°17'45''410
 -19°44'16''507   -44°17'52''079
-   -19°44'18''280 -44°17'52''079
+  -19°44'18''280 -44°17'52''079
 -19°44'18|280 -44°17'53''625
 -19°44'20''015 -44°17'53''625
 -19°44,20'zz015 -44°17'54@@984
-    -19°44'22''531 -44°17'54''984
+  -19°44'22''531 -44°17'54''984
 -19°44'22''531  zz-44°18'09''003
 -19°44xx30''662 -44°18'09''003
 -19°44'30''662 -44°18'19''307
 -19°44,,37''111 -44°18'19''307
 \zz-19°44'37''111 -44°17'41''703
--19°44'18''174 -44°17'41''703`; 
-    outext.value = 
-`-;019;44;18;174;-;044;17;41;703
--;019;44;18;174;-;044;17;45;410
--;019;44;16;507;-;044;17;45;410
--;019;44;16;507;-;044;17;52;079
--;019;44;18;280;-;044;17;52;079
--;019;44;18;280;-;044;17;53;625
--;019;44;20;015;-;044;17;53;625
--;019;44;20;015;-;044;17;54;984
--;019;44;22;531;-;044;17;54;984
--;019;44;22;531;-;044;18;09;003
--;019;44;30;662;-;044;18;09;003
--;019;44;30;662;-;044;18;19;307
--;019;44;37;111;-;044;18;19;307
--;019;44;37;111;-;044;17;41;703
--;019;44;18;174;-;044;17;41;703`;
+-19°44'18''174 -44°17'41''703`;    
+ 
+
+function onRefresh(){
+  if(localStorage.getItem('inputtext') == null){ /* first time page loaded */
+    inputtext.innerHTML = sample_inputfile;    
+    localStorage.setItem('inputtext', sample_inputfile);
+    ConvertnDraw(); // draw the initial graph and calculated output file
+  }
+  else{ /* refreshed by download or another action, just restore localStorage */
+    inputtext.value = localStorage.getItem('inputtext');
+    outputtext.value = localStorage.getItem('outputtext');
+    if(divgraph.childElementCount == 0) /* no graph */
+      ConvertnDraw(); // draw    
+  }  
 }
 
-function setOuput(outfile){
-    outext.value = outfile.text
-  }
+function ConvertnDraw() {
+  // true == gtmpro or false == auto
+  //var inputfmt = !document.getElementById('fmtgtmpro').checked; 
 
-function convert() {
-    fetch("/convert", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(intext.value)
+  fetch("/convert", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(inputtext.value)
+  })
+  .then(resp => {
+      if (resp.ok)
+        resp.json().then(data => {
+          if(data.result == 'success'){       
+            console.log(data.json_item);                      
+            //item = JSON.parse(data.json_item);        
+            document.getElementById("bokeh_plot").innerHTML = "";
+            Bokeh.embed.embed_item(data.json_item, "bokeh_plot"); // "bokeh_plot" target div id=bokeh_plot        
+          }
+          localStorage.setItem('outputtext', data.text); // save state of output text area
+          outputtext.value = data.text;  // set outputfile text even with. error msg   
+        });
     })
-    .then(resp => {
-        if (resp.ok)
-          resp.json().then(data => {
-            setOuput(data);
-          });
-      })
-    .catch(err => {
+  .catch(err => {
     console.log("An error occured", err.message);
     window.alert("Oops! Something went wrong.");
-    });
-  }
+  });
+}
 
-const fileUploader = document.getElementById('file-uploader');
 
 fileUploader.addEventListener('change', (event) => {
   const files = event.target.files;
@@ -70,25 +77,30 @@ fileUploader.addEventListener('change', (event) => {
   console.log('files', files);
 
   let reader = new FileReader();
-
   reader.readAsText(file);
 
-  reader.onload = function() {
-    intext.value = reader.result
-    console.log(reader.result);
+  reader.onload = function() {    
+    localStorage.setItem('inputtext', reader.result); // save state of input text area
+    inputtext.value = reader.result; 
+    console.log(reader.result);    
   };
-
 });
 
 
-function download(text) {
+// download : resets only output form data to converted state of left size
+function download(text){  
+  // save state of text areas
+  localStorage.setItem('inputtext', inputtext.value);
+  localStorage.setItem('outputtext', text);  
+
   var element = document.createElement('a');
   element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-  element.setAttribute('download', 'SIGAREAS.txt');
+  element.setAttribute('download', 'SIGAREAS.txt'); // filename 
 
   element.style.display = 'none';
   document.body.appendChild(element);
 
   element.click();
   document.body.removeChild(element);
+
 }
