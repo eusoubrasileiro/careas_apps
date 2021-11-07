28f357bb88a85f87db4697c59984f4f64bd548e0
André <eusoubrasileiro@gmail.com>
2021-11-07 14:27:15 -0300

    daemonized gunicorn workers

--- scripts/careas_apps_webserver.sh
+++ scripts/careas_apps_webserver.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 export ROOT=/home/andre/careas_apps
 source $ROOT/pythonvenv/bin/activate
-# stdout and stderr to trash since syslog will get logs
 # 8 workers for https
 cd $ROOT
-authbind  gunicorn -w 8 --certfile $ROOT/certs/fullchain.pem --keyfile $ROOT/certs/privkey.pem --bind 0.0.0.0:443  wsgi:app &> /dev/null
+authbind  gunicorn -D -w 8 --certfile $ROOT/certs/fullchain.pem --keyfile $ROOT/certs/privkey.pem --bind 0.0.0.0:443  wsgi:app 
 # 2 works for http  
-authbind  gunicorn -w 2 --bind 0.0.0.0:80  wsgi:app &> /dev/null
+authbind  gunicorn -D -w 2 --bind 0.0.0.0:80  wsgi:app 
 
