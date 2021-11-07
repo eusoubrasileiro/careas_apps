9ead724e25ad308a5aeef17c6766525dbc2995f7
André <eusoubrasileiro@gmail.com>
2021-11-07 14:09:40 -0300

    setup_webserver wrong path again

--- scripts/careas_apps_webserver.sh
+++ scripts/careas_apps_webserver.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 source /home/andre/careas_apps/pythonvenv/bin/activate
-authbind  gunicorn -w 8 --certfile certs/fullchain.pem --keyfile certs/privkey.pem --bind 0.0.0.0:443  wsgi:app 
-authbind  gunicorn -w 2 --bind 0.0.0.0:80  wsgi:app
+# stdout and stderr to trash since syslog will get logs
+# 8 workers for https
+authbind  gunicorn -w 8 --certfile certs/fullchain.pem --keyfile certs/privkey.pem --bind 0.0.0.0:443  wsgi:app >& /dev/null
+# 2 works for http  
+authbind  gunicorn -w 2 --bind 0.0.0.0:80  wsgi:app >& /dev/null
 
