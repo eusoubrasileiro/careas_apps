d7e2c1cad1d75dbadf4b25c0c2a4576e2463c314
André <eusoubrasileiro@gmail.com>
2021-11-07 13:58:38 -0300

    setup_webserver.sh only  use certbot if needed

--- scripts/setup_webserver.sh
+++ scripts/setup_webserver.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-sudo apt-get install python3-venv snapd -y
+sudo apt-get install python3-venv snapd authbind -y
+
 cd ~/careas_apps
 if [ ! -d "pythonvenv" ]; then 
     python3 -m venv pythonvenv
 fi 
 source pythonvenv/bin/activate
 pip install -r requeriments.txt
+
 if [ ! -d "aidbag" ]; then 
     git clone https://github.com/eusoubrasileiro/aidbag.git
 fi 
@@ -13,27 +15,27 @@ cd aidbag/anm/careas-pip/
 python setup.py install
 cd ../../../
 
-# to install certbot 
-# https://certbot.eff.org/lets-encrypt/ubuntufocal-nginx
-sudo snap install core; sudo snap refresh core
-sudo snap install --classic certbot
-sudo ln -s /snap/bin/certbot /usr/bin/certbot
-# create a standalone certificate port 80 must be open and domain server must pointing to this machine 
-sudo certbot certonly -d careas.iambr.xyz -n --standalone  --agree-tos --email eusoubrasileiro@gmail.com
-# create folder to store certificates created
 if [ ! -d "~/careas_apps/certs" ]; then # only if not created yet
+   # create folder to store certificates created
    mkdir "~/careas_apps/certs" 
+   # to install certbot 
+   # https://certbot.eff.org/lets-encrypt/ubuntufocal-nginx
+   sudo snap install core; sudo snap refresh core
+   sudo snap install --classic certbot
+   sudo ln -s /snap/bin/certbot /usr/bin/certbot
+   # create a standalone certificate port 80 must be open and domain server must pointing to this machine 
+   sudo certbot certonly -d careas.iambr.xyz -n --standalone  --agree-tos --email eusoubrasileiro@gmail.com
+
+   sudo cp /etc/letsencrypt/live/careas.iambr.xyz/fullchain.pem certs/
+   sudo cp /etc/letsencrypt/live/careas.iambr.xyz/privkey.pem certs/
+   sudo chown -R andre:andre certs/
 fi
-sudo cp /etc/letsencrypt/live/careas.iambr.xyz/fullchain.pem certs/
-sudo cp /etc/letsencrypt/live/careas.iambr.xyz/privkey.pem certs/
-sudo chown -R andre:andre certs/
 
 # careas_apps running by user andre
 # to allow run on port 80 HTTP and 443 HTTPS  
 # inside a python virtual env 
 # since sudo would not work properly
 # so passing access to specified user
-sudo apt-get install authbind
 sudo touch /etc/authbind/byport/80
 sudo chmod 500 /etc/authbind/byport/80
 sudo chown andre /etc/authbind/byport/80
