660485574cde05ad8d141d408886e213370fe919
André <eusoubrasileiro@gmail.com>
2021-11-07 14:21:25 -0300

    missing root path

--- scripts/careas_apps_webserver.sh
+++ scripts/careas_apps_webserver.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+export ROOT=/home/andre/careas_apps
 
-source /home/andre/careas_apps/pythonvenv/bin/activate
+source $ROOT/pythonvenv/bin/activate
 # stdout and stderr to trash since syslog will get logs
 # 8 workers for https
-authbind  gunicorn -w 8 --certfile certs/fullchain.pem --keyfile certs/privkey.pem --bind 0.0.0.0:443  wsgi:app &> /dev/null
+authbind  gunicorn -w 8 --certfile $ROOT/certs/fullchain.pem --keyfile $ROOT/certs/privkey.pem --bind 0.0.0.0:443  wsgi:app &> /dev/null
 # 2 works for http  
 authbind  gunicorn -w 2 --bind 0.0.0.0:80  wsgi:app &> /dev/null
 
