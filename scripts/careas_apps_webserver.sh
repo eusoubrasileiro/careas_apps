3082aecf70c4aa8bed38076c51f5a081bcefff28
André <eusoubrasileiro@gmail.com>
2021-11-09 13:37:33 -0300

    service webserver.sh fixing path for authbind

--- scripts/careas_apps_webserver.sh
+++ scripts/careas_apps_webserver.sh
@@ -3,7 +3,8 @@ export ROOT=/home/andre/careas_apps
 source $ROOT/pythonvenv/bin/activate
 # 8 workers for https
 cd $ROOT
-authbind  gunicorn -D -w 8 --certfile $ROOT/certs/fullchain.pem --keyfile $ROOT/certs/privkey.pem --bind 0.0.0.0:443  wsgi:app 
-# 2 works for http  
-authbind  gunicorn -D -w 2 --bind 0.0.0.0:80  wsgi:app 
-
+/usr/bin/authbind gunicorn -D -w 8 --certfile $ROOT/certs/fullchain.pem --keyfile $ROOT/certs/privkey.pem --bind 0.0.0.0:443  wsgi:app 
+# 2 workers for http  
+/usr/bin/authbind gunicorn -D -w 2 --bind 0.0.0.0:80  wsgi:app 
+# systemd was not made to work this way better have 2 services one for http and one for https
+# or use nginx
