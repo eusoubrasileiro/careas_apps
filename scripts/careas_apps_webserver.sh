412a380569a30b39776902180dc20844144a6245
André <eusoubrasileiro@gmail.com>
2021-11-24 14:46:36 -0300

    new execution script for http and https

--- scripts/careas_apps_webserver.sh
+++ scripts/careas_apps_webserver.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-export ROOT=/home/andre/careas_apps
-source $ROOT/pythonvenv/bin/activate
-# 8 workers for https
-cd $ROOT
-/usr/bin/authbind gunicorn -D -w 8 --certfile $ROOT/certs/fullchain.pem --keyfile $ROOT/certs/privkey.pem --bind 0.0.0.0:443  wsgi:app 
-# 2 workers for http  
-/usr/bin/authbind gunicorn -D -w 2 --bind 0.0.0.0:80  wsgi:app 
-# systemd was not made to work this way better have 2 services one for http and one for https
-# or use nginx
+
+# systemd was not made to work to call two commands
+# so let's  
+# replace by a Python script with two os.system
+
+c=`cat <<EOF
+import os 
+os.system(r"""source /home/andre/careas_apps/pythonvenv/bin/activate
+/usr/bin/authbind gunicorn -w 5 --certfile /home/andre/careas_apps/certs/fullchain.pem --keyfile /home/andre/careas_apps/certs/privkey.pem --bind 0.0.0.0:443  wsgi:app &""") 
+os.system("""source /home/andre/careas_apps/pythonvenv/bin/activate
+/usr/bin/authbind gunicorn -w 2 --bind 0.0.0.0:80  wsgi:app &""")
+EOF`
+python3 -c "$c"
+
+# 2 workers for http  and 5 https
+# /usr/bin/authbind gunicorn -D -w 2 --bind 0.0.0.0:80  wsgi:app 
+# systemd was not made to work this way better have 2 services one for http and one for https ?
+# or use nginx?
