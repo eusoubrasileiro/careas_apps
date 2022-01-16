ef616a7332cebc00e564829e459f3adb29181a82
André <eusoubrasileiro@gmail.com>
2022-01-16 15:00:08 -0300

    let nginx handle https ssl

--- scripts/careas_apps_webserver.py
+++ scripts/careas_apps_webserver.py
@@ -1,33 +1,15 @@
 #!/usr/bin/python3
 
-# systemd was not made to work to call .bash .sh with two commands
-# so this replaces that with two os.system with threading
-from threading import Thread
 import os 
-
-def gunicorn_https():
-    os.system("cd /home/andre/careas_apps \n"
-    "/usr/bin/authbind /home/andre/careas_apps/pythonvenv/bin/gunicorn -w 5 "
-    "--certfile /home/andre/careas_apps/certs/fullchain.pem --keyfile /home/andre/careas_apps/certs/privkey.pem "
-    "--bind 0.0.0.0:443 --access-logfile=/home/andre/careas_apps/access_https.log "
-    "--capture-output --enable-stdio-inheritance "
-    "wsgi:app")
 
 def gunicorn_http():
     os.system("cd /home/andre/careas_apps \n"
     "/usr/bin/authbind /home/andre/careas_apps/pythonvenv/bin/gunicorn -w 5 "    
-    "--bind 0.0.0.0:80 --access-logfile=/home/andre/careas_apps/access_http.log "
+    "--bind 0.0.0.0:5000 --access-logfile=/home/andre/careas_apps/access_http.log "
     "--capture-output --enable-stdio-inheritance "
     "wsgi:app")
 
-#--access-logfile=/logs/rest.app/access.log
-
-t_http = Thread(target=gunicorn_http)
-t_https = Thread(target=gunicorn_https)
-t_http.start()
-t_https.start()
-t_http.join()
-t_https.join()
+gunicorn_http()
 # or /home/andre/careas_apps/pythonvenv/bin/python
 # 2 workers for http  and 5 https
 # /usr/bin/authbind gunicorn -D -w 2 --bind 0.0.0.0:80  wsgi:app 
