e47a5dae338d45fec5ebfbf0ae2dcae40f639241
André <eusoubrasileiro@gmail.com>
2022-02-20 16:25:07 -0300

    new react site now in production certbot ssl automated every 30 days

--- scripts/setup_webserver.sh
+++ scripts/setup_webserver.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-sudo apt-get install python3.8-venv snapd authbind -y
+sudo apt-get install python3.8-venv authbind -y
 
 cd ~/careas_apps
 if [ ! -d ~/careas_apps/pythonvenv ]; then 
     python3 -m venv pythonvenv
 fi 
 source pythonvenv/bin/activate
-pip install -r requeriments.txt
+pip install -r backend/requeriments.txt
 
 if [ ! -d ~/careas_apps/aidbag ]; then 
     git clone https://github.com/eusoubrasileiro/aidbag.git
@@ -15,30 +15,23 @@ cd aidbag/anm/careas-pip/
 python setup.py install
 cd ../../../
 
-if [ ! -d ~/careas_apps/certs ]; then # only if not created yet
-   # create folder to store certificates created
-   mkdir "~/careas_apps/certs" 
-   # to install certbot 
-   # https://certbot.eff.org/lets-encrypt/ubuntufocal-nginx
-   sudo snap install core; sudo snap refresh core
-   sudo snap install --classic certbot
-   sudo ln -s /snap/bin/certbot /usr/bin/certbot
-   # create a standalone certificate port 80 must be open and domain server must pointing to this machine 
-   sudo certbot certonly -d careas.iambr.xyz -n --standalone  --agree-tos --email eusoubrasileiro@gmail.com
-
-   sudo cp /etc/letsencrypt/live/careas.iambr.xyz/fullchain.pem certs/
-   sudo cp /etc/letsencrypt/live/careas.iambr.xyz/privkey.pem certs/
-   sudo chown -R andre:andre certs/
+if [ ! -d /etc/letsencrypt/live/careas.iambr.xyz ]; then # only if not created yet
+   # https://phoenixnap.com/kb/letsencrypt-nginx
+   sudo apt install certbot python3-certbot-nginx -y
+   # create a standalone certificate port 80/443 must be open and domain server must pointing to this machine 
+   # also server_name must be set to careas.iambr.xyz
+   sudo certbot --nginx -d careas.iambr.xyz 
 fi
 
 # careas_apps running by user andre (better control not root)
 # to allow run on port 8000 (not 80 to allow nginx control) 
 # HTTPS and forward will be dealt by nginx
 # so passing access to specified user
-sudo touch /etc/authbind/byport/8000
-sudo chmod 500 /etc/authbind/byport/8000
-sudo chown andre /etc/authbind/byport/8000
+sudo touch /etc/authbind/byport/5000
+sudo chmod 500 /etc/authbind/byport/5000
+sudo chown andre /etc/authbind/byport/5000
 
+#/usr/bin/authbind /home/andre/careas_apps/pythonvenv/bin/gunicorn -w 4 --bind 0.0.0.0:5000 main:app
 # service still runs on my current user
 sudo systemctl stop careas_apps_webserver
 sudo cp ~/careas_apps/scripts/careas_apps_webserver.service /etc/systemd/system/
@@ -47,8 +40,7 @@ sudo systemctl daemon-reload
 sudo systemctl enable careas_apps_webserver
 sudo systemctl start careas_apps_webserver
 
-cat ~/careas_apps/scripts/sites_nginx.conf >> /etc/nginx/sites-enabled/sites_nginx.conf
-
+sudo cp ~/careas_apps/scripts/careas_apps_react.conf /etc/nginx/sites-enabled/
 sudo systemctl restart nginx
 
 # namecheap dynamic dns better allways on since ISP may not be reliable for home-internet service
