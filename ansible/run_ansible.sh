16cbbe957d1aeb95020360ea06461945dce3fd41
eusoubrasileiro <eusoubrasileiro@gmail.com>
2024-02-13 21:34:24 -0300

    loophole .service to start at every boot

--- ansible/run_ansible.sh
+++ ansible/run_ansible.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # to run ansible from another machine and not ssh default to current user
 # use root:orangepi02 dont need become 
-ansible-playbook orangepi02.yaml --user root --ask-pass
+ansible-playbook orangepi02.yaml --user root --ask-pass -tags services,nfs
