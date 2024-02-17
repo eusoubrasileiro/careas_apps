6f33c771dcc0d810d41d461090612f7827c52533
eusoubrasileiro <eusoubrasileiro@gmail.com>
2024-02-16 21:54:02 -0300

    react app working on careas.loophole.site - fixed ansible now update is quite simple - nginx also made clean

--- ansible/run_ansible.sh
+++ ansible/run_ansible.sh
@@ -2,3 +2,6 @@
 # to run ansible from another machine and not ssh default to current user
 # use root:orangepi02 dont need become 
 ansible-playbook orangepi02.yaml --user root --ask-pass -tags services,nfs
+
+# to update webserver 
+# ansible-playbook orangepi02.yaml --user root --ask-pass --tags services,webserver
